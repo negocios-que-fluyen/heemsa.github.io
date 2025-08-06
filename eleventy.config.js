b06d3558917d56df311deee95901950a47204b1f
Andres MG <negociosquefluyen@gmail.com>
2025-08-06 15:11:34 -0600

    se arreglan los paths para el proyecto

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,11 +6,11 @@ module.exports = function(eleventyConfig) {
   
   return {
     dir: {
-      input: "_pages",
+      input: ".",
       output: "_site",
-      includes: "../_includes",
-      layouts: "../_layouts",
-      data: "../_data"
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data"
     }
   };
 };
